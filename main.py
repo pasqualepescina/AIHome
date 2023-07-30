c90ecb766dc6f6966d534d5bcf7b8f91e4ae20cb
Pasquale Pescina <pescinapasquale30@gmail.com>
2023-07-30 16:02:12 +0200

    logs y llamada

--- main.py
+++ main.py
@@ -1,43 +1,51 @@
-import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_web(urls):
-    scraped_data = []
-    for url in urls:
-        try:
-            # Send an HTTP GET request to the URL
-            response = requests.get(url)
+def scrape_annuncio_items(url):
+    try:
+        # Send an HTTP GET request to the URL
+        response = requests.get(url)
 
-            # Check if the request was successful
-            if response.status_code == 200:
-                # Parse the content of the web page using BeautifulSoup
-                soup = BeautifulSoup(response.content, 'html.parser')
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Parse the content of the web page using BeautifulSoup
+            soup = BeautifulSoup(response.content, 'html.parser')
 
-                # Extract all the HTML tags and their corresponding text
-                html_labels = [tag.name for tag in soup.find_all()]
-                scraped_text = [tag.get_text() for tag in soup.find_all()]
+            # Find all the listings with class "annuncio-item" on the page
+            annuncio_items = soup.find_all()
 
-                # Combine the HTML labels and text into a DataFrame
-                df = pd.DataFrame({'HTML_Label': html_labels, 'Text': scraped_text})
+            # Return the list of scrapped data
+            return annuncio_items
 
-                # Append the DataFrame to the list of scraped data
-                scraped_data.append(df)
+        else:
+            print(f"Failed to scrape {url}. Status code: {response.status_code}")
+            return None
 
-            else:
-                print(f"Failed to scrape {url}. Status code: {response.status_code}")
+    except Exception as e:
+        print(f"Error while scraping {url}: {str(e)}")
+        return None
 
-        except Exception as e:
-            print(f"Error while scraping {url}: {str(e)}")
+def save_to_txt(file_path, data):
+    try:
+        with open(file_path, 'w', encoding='utf-8') as file:
+            for item in data:
+                file.write(str(item) + "\n")
+                file.write("-" * 40 + "\n")
 
-    return scraped_data
+        print(f"Scraped data saved to {file_path}")
 
-# Example usage:
-urls_to_scrape = ['https://example.com/page1', 'https://example.com/page2', 'https://example.com/page3']
-scraped_results = scrape_web(urls_to_scrape)
+    except Exception as e:
+        print(f"Error while saving to {file_path}: {str(e)}")
 
-# Concatenate the list of DataFrames into a single DataFrame
-final_df = pd.concat(scraped_results, ignore_index=True)
+# URL to scrape
+url_to_scrape = 'https://www.tecnocasa.it/annunci/immobili/lombardia/milano.html'
 
-# Print the resulting DataFrame
-print(final_df)
+# Scrape the data
+scraped_annuncio_items = scrape_annuncio_items(url_to_scrape)
+
+# Save the scrapped data to a text file
+if scraped_annuncio_items is not None:
+    file_path = 'scraped_data_tecnocasa.txt'
+    save_to_txt(file_path, scraped_annuncio_items)
+else:
+    print("Failed to scrape or no data found.")
